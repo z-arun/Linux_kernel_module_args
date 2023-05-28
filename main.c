c3f50dc5d9ea00a902bf1745ab0226d3f2426cfa
Arun S Nair <111906073+z-arun@users.noreply.github.com>
2023-05-28 23:09:51 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
-/
+#include<linux/module.h>
+#include<linux/init.h>
+#include<linux/kernel.h>
+#include<linux/moduleparam.h>
+#include<linux/stat.h>
+
+
+MODULE_LICENSE("GPL");
+
+static int argument = 0;
+module_param(argument,int,S_IRUSR |S_IWUSR);
+MODULE_PARM_DESC(argument,"test int parameter");
+
+int __init startFun(void)
+{
+	printk(KERN_INFO "Hello world %d",argument);
+	return 0;
+}
+
+void __exit endFun(void)
+{
+	printk(KERN_INFO "bye world");
+}
+
+module_init(startFun);
+module_exit(endFun);
